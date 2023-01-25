21329c9e636bc7cb9c9ed99ee37c73f085e9406f
Gul Akberova <kbrovagul0@gmail.com>
2023-01-25 08:59:42 +0400

    mui table

--- src/category/CategoriesQuery.jsx
+++ src/category/CategoriesQuery.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from "axios";
 import { useQuery } from "react-query";
-
+import { DataGrid } from '@mui/x-data-grid';
 function CategoriesQuery() {
     const { data, isLoading, isError, error, isFetching } = useQuery(
         "categories",
@@ -9,13 +9,21 @@ function CategoriesQuery() {
           return axios
             .get("https://northwind.vercel.app/api/categories")
             .then((res) => res.data);
+            console.log(res.data);
         },
         {
           //   cacheTime: 5000,
           //   staleTime: 15000,
         }
       );
-    
+      const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'name', headerName: 'Name', width: 130 },
+        { field: 'description', headerName: 'Description', width: 130 },
+       
+      ];
+   
+   
       console.log(`isLoading ${isLoading} isFetching ${isFetching}`);
     
       if (isLoading) return <h1>loading ...</h1>;
@@ -23,14 +31,19 @@ function CategoriesQuery() {
       if (isFetching) return <h1>updating ...</h1>;
     
       if (isError) return <h1>{error.message}</h1>;
-      
+
   return (
     <>
-      <div>
-      {data?.map((category, index) => (
-        <p key={index}>{category.name}</p>
-      ))}
+      <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={data}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
     </div>
+    
     
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
- 
-const queryClient = new QueryClient()
+import { StyledEngineProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 // import { ReactQueryDevtools } from "react-query/devtools";
-
 
 const client = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={client}>
-    <App />
-   </QueryClientProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+ 
+    <QueryClientProvider client={client}>
+       <StyledEngineProvider injectFirst>
+       <App />
+       </StyledEngineProvider>
+     
+    </QueryClientProvider>
+);
